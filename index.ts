0f53073262f47dc979d135ecadf0babddf6e48e9
dhiajlali <135439641+dhiajlali@users.noreply.github.com>
2023-08-13 09:39:14 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -2,5 +2,7 @@
 //###########################           SSTech       ###################
 //###########################   Programming Courses  ###################
 //######################################################################
-let index : number
-for(index=1;index=<100;index++){console.log("allh akbar",index)};
+let index: number;
+for (index = 1; index <= 100; index++) {
+  console.log('allh akbar', index);
+}
